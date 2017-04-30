12cef3e98d551c26f7de00b2ab10f70ec80a200e
yuhau lin <yuhau lin>
2017-04-29 23:39:07 -0400

    Changed truth recorder to an html page

--- Audio Keylogger/tools/record truth.js
+++ Audio Keylogger/tools/record truth.js
@@ -1,12 +1,37 @@
 var truth = {
-	"down": [],
-	"up": []
+	'down': [],
+	'up': []
 };
 $(document).on('keydown', (e) => truth.down.push({
-	"key": e.key,
-	"t": e.timeStamp
-}));
+		'key': e.key,
+		't': e.timeStamp
+	}));
 $(document).on('keyup', (e) => truth.up.push({
-	"key": e.key,
-	"t": e.timeStamp
-}));
+		'key': e.key,
+		't': e.timeStamp
+	}));
+
+textFile = null;
+makeTextFile = function(text) {
+	var data = new Blob([text], {
+			type: 'text/plain'
+		});
+
+	// If we are replacing a previously generated file we need to
+	// manually revoke the object URL to avoid memory leaks.
+	if (textFile !== null) {
+		window.URL.revokeObjectURL(textFile);
+	}
+
+	textFile = window.URL.createObjectURL(data);
+
+	// returns a URL you can use as a href
+	return textFile;
+};
+
+save = function() {
+	var link = document.createElement('a');
+	link.setAttribute('download', 'info.txt');
+	link.href = makeTextFile(truth.down.map(e => e.key + ',' + e.t).join('\n'));
+	link.dispatchEvent(new MouseEvent('click'));
+}
