b251575cf13bd90cf0fc0c7f7aae964277ede480
Harish Lall <github@harishlall.com>
2017-04-30 11:06:35 -0400

    switch to training data

--- Audio Keylogger/src/keylogger.py
+++ Audio Keylogger/src/keylogger.py
@@ -4,18 +4,20 @@ import argparse
 import string
 from features import *
 from keystrokes import *
+from data_labeler import *
 
 from sklearn import svm
 
 def keylogger(args):
 	
-	training_keystrokes = keystroke_starts(args.training_wav)
-	training_features = extract_features(args.training_wav, training_keystrokes)
+	#training_keystrokes = keystroke_starts(args.training_wav)
+	#training_features = extract_features(args.training_wav, training_keystrokes)
+	training_features, training_keystrokes = get_training_set(args.training_wav, args.training_truth)
 	input_keystrokes = keystroke_starts(args.input_wav)
 	input_features = extract_features(args.input_wav, input_keystrokes)
 
 	classifier = svm.SVC()
-	classifier.fit(training_features[5:31], list(string.ascii_lowercase))
+	classifier.fit(training_features, training_keystrokes)
 	prediction = classifier.predict(input_features)
 
 	print('Predicted sample: {}'.format(str(prediction)))
